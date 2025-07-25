2bae5a5511e1b4b2bbcfb7c1202d261c40f548f2
BruhLemma-Yadecha <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2025-07-25 18:29:14 -0400

    chore: regenerate lock

--- backend/optimizer/views.py
+++ backend/optimizer/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
+
 from rocketmancer import Rocket, SolverError, Stage
 
 
